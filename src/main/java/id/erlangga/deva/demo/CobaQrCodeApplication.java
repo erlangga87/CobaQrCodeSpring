f358550db2d395fcf1d3e19559e408f10eed5fe0
erlangga87 <deva.erlangga87@gmail.com>
2021-06-18 14:24:54 +0700

    coba qr code

---
+++ src/main/java/id/erlangga/deva/demo/CobaQrCodeApplication.java
@@ -0,0 +1,13 @@
+package id.erlangga.deva.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobaQrCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobaQrCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/erlangga/deva/demo/CobaQrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.erlangga.deva.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobaQrCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
